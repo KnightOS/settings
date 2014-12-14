d85e430c16426d8dbf074082154820556d66511b
Willem Sonke <w.m.sonke@student.tue.nl>
2014-12-14 20:03:15 +0100

    Add a window and a Back option
    The next step is to fill the window with content...

--- main.asm
+++ main.asm
@@ -190,7 +190,25 @@ _:  pcall(fastCopy)
     jr .writeVersion
 
 setDateTime:
-    ; TODO
+    pcall(clearBuffer)
+    
+    kld(hl, windowTitle)
+    xor a
+    corelib(drawWindow)
+    
+    kld(hl, backStr)
+    ld de, 0x0632
+    push de
+        pcall(drawStr)
+    pop de
+    ld d, 2
+    kld(hl, caretIcon)
+    pcall(putSpriteOR)
+
+_:  pcall(fastCopy)
+    pcall(flushKeys)
+    corelib(appWaitKey)
+    jr nz, -_
     ret
     
 exit:
