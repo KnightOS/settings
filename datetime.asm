a10dd7c42ca2be9d6b6b396a57403187dccbb8d1
Willem Sonke <w.m.sonke@student.tue.nl>
2014-12-14 21:19:19 +0100

    Add instruction message
    Also did some renaming of variables to make them clearer, and inserted a variable to hold the currently selected field.

--- datetime.asm
+++ datetime.asm
@@ -10,23 +10,30 @@ setDateTime:
     cp errUnsupported
     kjp(z, unsupported)
     
-    kld((selected_year), ix)
+    kld((current_year), ix)
     ld a, l
-    kld((selected_month), a)
+    kld((current_month), a)
     ld a, h
-    kld((selected_day), a)
+    kld((current_day), a)
     ld a, b
-    kld((selected_hour), a)
+    kld((current_hour), a)
     ld a, c
-    kld((selected_minute), a)
+    kld((current_minute), a)
     ld a, d
-    kld((selected_second), a)
+    kld((current_second), a)
     
     pcall(clearBuffer)
 
     kld(hl, windowTitle)
     xor a
     corelib(drawWindow)
+    
+    ; draw the instruction message
+    ld de, 0x0208
+    ld b, 2
+
+    kld(hl, instruction_message)
+    pcall(drawStr)
     
     ; draw the date/time
     .drawDate:
@@ -35,7 +42,7 @@ setDateTime:
     ; is not implemented yet)
     
     push hl
-        kld(hl, (selected_year))
+        kld(hl, (current_year))
         
         ; subtract 1900 from it and put the result in a
         ld de, -1900
@@ -44,83 +51,79 @@ setDateTime:
         
         ; if a >= 100 we have year "20.."
         cp 100
-        ld de, 0x0208
+        ld de, 0x0220
         jr nc, .drawYear2000
 
 .drawYear1900:
-        ld de, 0x0a08
+        ld de, 0x0a20
         kcall(drawDecAPadded)
         
         ; "19"
         ld a, '1'
-        ld de, 0x0208
+        ld de, 0x0220
         pcall(drawChar)
         ld a, '9'
-        ld de, 0x0608
+        ld de, 0x0620
         pcall(drawChar)
         
         jr .endDrawYear
 
 .drawYear2000:
         sub 100
-        ld de, 0x0a08
+        ld de, 0x0a20
         kcall(drawDecAPadded)
         
         ; "20"
         ld a, '2'
-        ld de, 0x0208
+        ld de, 0x0220
         pcall(drawChar)
         ld a, '0'
-        ld de, 0x0608
+        ld de, 0x0620
         pcall(drawChar)
 
 .endDrawYear:
     pop hl
     
     ; now, draw the month and day
-    kld(a, (selected_month))
+    kld(a, (current_month))
     inc a
-    ld de, 0x1608
+    ld de, 0x1620
     kcall(drawDecAPadded)
-    kld(a, (selected_day))
+    kld(a, (current_day))
     inc a
-    ld de, 0x2208
+    ld de, 0x2220
     kcall(drawDecAPadded)
     
     ; the dashes
     ld a, '-'
-    ld de, 0x1208
+    ld de, 0x1220
     pcall(drawChar)
-    ld de, 0x1e08
+    ld de, 0x1e20
     pcall(drawChar)
 
 .drawTime:
-    kld(a, (selected_hour))
-    ld de, 0x0210
+    kld(a, (current_hour))
+    ld de, 0x3220
     kcall(drawDecAPadded)
-    kld(a, (selected_minute))
-    ld de, 0x0c10
-    kcall(drawDecAPadded)
-    kld(a, (selected_second))
-    ld de, 0x1610
+    kld(a, (current_minute))
+    ld de, 0x3c20
     kcall(drawDecAPadded)
     
-    ; the colons
+    ; the colon
     ld a, ':'
-    ld de, 0x0a10
-    pcall(drawChar)
-    ld de, 0x1410
+    ld de, 0x3a20
     pcall(drawChar)
     
-    kld(hl, backStr)
-    ld de, 0x0632
-    push de
-        pcall(drawStr)
-    pop de
-    ld d, 2
-    ld b, 5
-    kld(hl, caretIcon)
+.drawSelection:
+    ld de, 0x061c
+    ld b, 3
+    kld(hl, caretUpIcon)
     pcall(putSpriteOR)
+    ld de, 0x0626
+    ld b, 3
+    kld(hl, caretDownIcon)
+    pcall(putSpriteOR)
+    
 
 _:  pcall(fastCopy)
     pcall(flushKeys)
@@ -156,21 +159,39 @@ drawDecAPadded:
     ret
 
 ; variables
-selected_year:
+current_year:
     .db 0, 0
-selected_month:
+current_month:
     .db 0
-selected_day:
+current_day:
     .db 0
-selected_hour:
+current_hour:
     .db 0
-selected_minute:
+current_minute:
     .db 0
-selected_second:
+current_second:
+    .db 0
+selected_field: ; 0 = year, 1 = month, 2 = day, 3 = hour, 4 = minute
     .db 0
 
+; constants
 clock_unsupported_message:
     .db "Clock isn't sup-\nported on this\ncalculator.", 0
 clock_unsupported_options:
     .db 1
-    .db "Quit program", 0
+    .db "Back", 0
+
+instruction_message:
+    .db "Use the arrow keys to set the\nclock and press Enter to\nsave or Clear to cancel.", 0
+
+selected_field_indicator_x:
+    .db 6, 14, 20, 35, 41
+
+caretUpIcon:
+    .db 0b00010000
+    .db 0b00111000
+    .db 0b01111100
+caretDownIcon:
+    .db 0b01111100
+    .db 0b00111000
+    .db 0b00010000
